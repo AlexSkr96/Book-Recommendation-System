ab09b163fd64bd260b0a3a6e0ac8a8b5fde074c6
Alex <github.tacky872@passmail.net>
2025-02-19 21:21:10 +0300

    Minor updates

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 import requests
+import json
 
 
 parameters = "q=catch22"
-contents = requests.get(f"https://openlibrary.org/search.json?{parameters}")
-print(contents.content)
+response = requests.get(f"https://openlibrary.org/search.json?{parameters}")
+response_dict = json.loads(response.content)
+print(response_dict['docs'][0]['ia'])
+
+# url = "https://api.archivelab.org/v1/books"
+# headers = {"accept": "application/json"}
+# response = requests.get(url, headers=headers)
+# print(response.text)
